7c615e658d7330090e3c13c8c15db42fa590519c
Jenni Peltola <jenni.peltola@hotmail.com>
2026-02-03 10:40:54 +0200

    Osa01, harjoitus 1.7

--- osa01/unicafe/App/src/App.jsx
+++ osa01/unicafe/App/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
+  const positive = (good / total) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -15,6 +19,9 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>total {total}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
     </div>
   )
 }
