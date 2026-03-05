0ccac4a0ad7f1c0b443455daa62b71eac8ba0df9
Jenni Peltola <jenni.peltola@tuni.fi>
2026-03-05 13:08:58 +0200

    Osa02, harjoitus 2.6

---
+++ osa02/puhelinluettelo/App/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  // Handles adding a new name to the array
+  const addName = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  // Handles changes for input value
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        <ul>
+          {persons.map(person =>
+            <li key={person.name}>{person.name}</li>
+          )}
+        </ul>
+    </div>
+  )
+
+}
+
+export default App
